3670d14b07af9fc5afe6683a72675444ac466440
jdc6490 <32806347+jdc6490@users.noreply.github.com>
2019-02-13 18:39:06 -0600

    adding a console.log to branch1

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  console.log('This is an edit to branch1');
+
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
