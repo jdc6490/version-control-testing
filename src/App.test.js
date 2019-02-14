3216cef1ca9b53e32d8335084b6d434330b5fd66
jdc6490 <32806347+jdc6490@users.noreply.github.com>
2019-02-13 18:48:26 -0600

    commiting a branch2

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  console.log('This is an edit to branch1');
+  console.log('This is an edit to branch2');
 
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
